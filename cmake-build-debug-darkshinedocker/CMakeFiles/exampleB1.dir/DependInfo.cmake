40c1f9743754895aba2ef2ecae6a9618255b2e31
xinzhuwang-wxz <155371353+xinzhuwang-wxz@users.noreply.github.com>
2024-01-23 21:55:40 +0800

    Add files via upload

---
+++ cmake-build-debug-darkshinedocker/CMakeFiles/exampleB1.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/tmp/B1-2/exampleB1.cc" "/tmp/B1-2/cmake-build-debug-darkshinedocker/CMakeFiles/exampleB1.dir/exampleB1.cc.o"
+  "/tmp/B1-2/src/B1ActionInitialization.cc" "/tmp/B1-2/cmake-build-debug-darkshinedocker/CMakeFiles/exampleB1.dir/src/B1ActionInitialization.cc.o"
+  "/tmp/B1-2/src/B1DetectorConstruction.cc" "/tmp/B1-2/cmake-build-debug-darkshinedocker/CMakeFiles/exampleB1.dir/src/B1DetectorConstruction.cc.o"
+  "/tmp/B1-2/src/B1EventAction.cc" "/tmp/B1-2/cmake-build-debug-darkshinedocker/CMakeFiles/exampleB1.dir/src/B1EventAction.cc.o"
+  "/tmp/B1-2/src/B1PrimaryGeneratorAction.cc" "/tmp/B1-2/cmake-build-debug-darkshinedocker/CMakeFiles/exampleB1.dir/src/B1PrimaryGeneratorAction.cc.o"
+  "/tmp/B1-2/src/B1RunAction.cc" "/tmp/B1-2/cmake-build-debug-darkshinedocker/CMakeFiles/exampleB1.dir/src/B1RunAction.cc.o"
+  "/tmp/B1-2/src/B1SteppingAction.cc" "/tmp/B1-2/cmake-build-debug-darkshinedocker/CMakeFiles/exampleB1.dir/src/B1SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/opt/geant4/include/Geant4"
+  "/usr/include/aarch64-linux-gnu/qt5"
+  "/usr/include/aarch64-linux-gnu/qt5/QtGui"
+  "/usr/include/aarch64-linux-gnu/qt5/QtCore"
+  "/usr/lib/aarch64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/aarch64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/aarch64-linux-gnu/qt5/QtPrintSupport"
+  "/usr/include/aarch64-linux-gnu/qt5/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
